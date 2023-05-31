d0c8d5b6b3c0f2ab93dd74a501ad8ed6022177bb
ferrarajohn <94008222+ferrarajohn@users.noreply.github.com>
2023-05-31 01:05:43 -0500

    Update Showcase.jsx

--- src/components/home/showcase/Showcase.jsx
+++ src/components/home/showcase/Showcase.jsx
@@ -131,83 +131,83 @@ const Showcase = () => {
       </div>
      </div>
 
-      <div className="second-main-container">
-        <div className="flex-container">
-          <div className="second-main-title">
-            <h1>RESOURCES</h1>
-          </div>
-          <div className="first-item">
-            <div className="item1">
-              <p>
-                How to link an item: Navigate to the Link page, upload an image of your item into the box on the left,
-                enter other information in the other box, and press "Link item!" It may take a few minutes for your item
-                to finish linking.
-              </p>
-            </div>
-            <div className="item2">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, voluptatibus?Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Quas, laborum.
-              </p>
-            </div>
-            <div className="item3">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, qui.Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Quas, laborum.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="second-item">
-          <div className="image1">
-            <img
-              className="second-item-image"
-              src="https://i.seadn.io/gae/2QsFxb14us6FSDu_Rkj4cXflv_ijntnySy_a3jDah0fS5TbszBKbye4wek0aNHWGZqkeQ1CmlqNUJf9TEIel7tcmFgTvPTK-dVAWmXM?auto=format&w=3840"
-              alt=""
-            />
-          </div>
-          <div className="image2">
-            <img
-              className="second-item-image"
-              src="https://i.seadn.io/gae/X4Q2rHfwRSUHRw9HLkKfG3J-8NjjhUTXV3sJ1CVvCtg-ycL-hHroQMiEUP9pqRNJvV7mjH2euA1jDxKvS1mhgQI-ZUljlhI-owP2GA?auto=format&w=3840"
-              alt=""
-            />
-          </div>
-          <div className="image3">
-            <img
-              className="second-item-image"
-              src="https://i.seadn.io/gae/ibjUDVjAvvT4yN5uuplWVSL7q9SD3WdDhJWkpdkHtT-NkOCdnKIuFVzdntP4sQ2BcLyf2I2hwFI-IdaxUOqXBZ6g0jS_jcR9ud2Syg?auto=format&w=3840"
-              alt=""
-            />
-          </div>
-          <div className="image4">
-            <img
-              className="second-item-image"
-              src="https://i.seadn.io/gae/YwG6cplD6vUrJzksp-BXVeYhQfroGPt0OVmXf-6t9dnblRkfgGdj_YUMXlPTqfzWnKwovYHRD3ud0zO_KMcadflBqcBsfdUm6YpOqA?auto=format&w=3840"
-              alt=""
-            />
-            <button className="btn">
-              <a href="/link">Link a Code</a>
-            </button>
-          </div>
-        </div>
-      </div>
+//       <div className="second-main-container">
+//         <div className="flex-container">
+//           <div className="second-main-title">
+//             <h1>RESOURCES</h1>
+//           </div>
+//           <div className="first-item">
+//             <div className="item1">
+//               <p>
+//                 How to link an item: Navigate to the Link page, upload an image of your item into the box on the left,
+//                 enter other information in the other box, and press "Link item!" It may take a few minutes for your item
+//                 to finish linking.
+//               </p>
+//             </div>
+//             <div className="item2">
+//               <p>
+//                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, voluptatibus?Lorem, ipsum dolor sit amet
+//                 consectetur adipisicing elit. Quas, laborum.
+//               </p>
+//             </div>
+//             <div className="item3">
+//               <p>
+//                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, qui.Lorem, ipsum dolor sit amet
+//                 consectetur adipisicing elit. Quas, laborum.
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="second-item">
+//           <div className="image1">
+//             <img
+//               className="second-item-image"
+//               src="https://i.seadn.io/gae/2QsFxb14us6FSDu_Rkj4cXflv_ijntnySy_a3jDah0fS5TbszBKbye4wek0aNHWGZqkeQ1CmlqNUJf9TEIel7tcmFgTvPTK-dVAWmXM?auto=format&w=3840"
+//               alt=""
+//             />
+//           </div>
+//           <div className="image2">
+//             <img
+//               className="second-item-image"
+//               src="https://i.seadn.io/gae/X4Q2rHfwRSUHRw9HLkKfG3J-8NjjhUTXV3sJ1CVvCtg-ycL-hHroQMiEUP9pqRNJvV7mjH2euA1jDxKvS1mhgQI-ZUljlhI-owP2GA?auto=format&w=3840"
+//               alt=""
+//             />
+//           </div>
+//           <div className="image3">
+//             <img
+//               className="second-item-image"
+//               src="https://i.seadn.io/gae/ibjUDVjAvvT4yN5uuplWVSL7q9SD3WdDhJWkpdkHtT-NkOCdnKIuFVzdntP4sQ2BcLyf2I2hwFI-IdaxUOqXBZ6g0jS_jcR9ud2Syg?auto=format&w=3840"
+//               alt=""
+//             />
+//           </div>
+//           <div className="image4">
+//             <img
+//               className="second-item-image"
+//               src="https://i.seadn.io/gae/YwG6cplD6vUrJzksp-BXVeYhQfroGPt0OVmXf-6t9dnblRkfgGdj_YUMXlPTqfzWnKwovYHRD3ud0zO_KMcadflBqcBsfdUm6YpOqA?auto=format&w=3840"
+//               alt=""
+//             />
+//             <button className="btn">
+//               <a href="/link">Link a Code</a>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
 
-      <div className="news-container">
-        <div className="show-btn-wrap">
-          <Link to="/link" className="btn showcase-btn light-btn">
-            Get Codes
-          </Link>
-        </div>
-        <div className="email-container">
-          <h1 className="news-title">Sign up for updates on Delorean!</h1>
-          <div className="news-flex">
-            <input className="email" type="email" id="email" name="email" placeholder="Enter your e-mail." />
-            <input className="submit" type="submit" />
-          </div>
-        </div>
-      </div>
-    </div>
+//       <div className="news-container">
+//         <div className="show-btn-wrap">
+//           <Link to="/link" className="btn showcase-btn light-btn">
+//             Get Codes
+//           </Link>
+//         </div>
+//         <div className="email-container">
+//           <h1 className="news-title">Sign up for updates on Delorean!</h1>
+//           <div className="news-flex">
+//             <input className="email" type="email" id="email" name="email" placeholder="Enter your e-mail." />
+//             <input className="submit" type="submit" />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
   );
 };
 
